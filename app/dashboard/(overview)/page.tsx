382b5704290dd3073bff0bbcb307a0ff8755404c
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-08-19 18:38:15 -0400

    add suspense in the latest envoices

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,7 +4,7 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchCardData, fetchLatestInvoices } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 
 export default async function Page() {
     // const revenue = await fetchRevenue();
@@ -29,7 +29,9 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton/>}>
         <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton/>}>
+        <LatestInvoices/>
+        </Suspense>
       </div>
     </main>
   );
